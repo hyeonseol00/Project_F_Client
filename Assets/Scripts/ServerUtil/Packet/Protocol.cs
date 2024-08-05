3bfcd3786ac69d52a9813797b224f47ff31a047a
hyeonseol00 <hyeonseol22@naver.com>
2024-08-05 12:48:25 +0900

    ✨ update : 둥지 입장 패킷에 보스 위치 정보 추가

--- Assets/Scripts/ServerUtil/Packet/Protocol.cs
+++ Assets/Scripts/ServerUtil/Packet/Protocol.cs
@@ -87,33 +87,34 @@ namespace Google.Protobuf.Protocol {
             "c3VjY2VzcxgBIAEoCBIPCgdtZXNzYWdlGAIgASgJIi0KB0NfTG9nSW4SEAoI",
             "bmlja25hbWUYASABKAkSEAoIcGFzc3dvcmQYAiABKAkiKwoHU19Mb2dJbhIP",
             "CgdzdWNjZXNzGAEgASgIEg8KB21lc3NhZ2UYAiABKAkiEQoPQ19FbnRlckhh",
-            "dGNoZXJ5IlwKD1NfRW50ZXJIYXRjaGVyeRIkCgZwbGF5ZXIYASABKAsyFC5Q",
-            "cm90b2NvbC5QbGF5ZXJJbmZvEhEKCWJvc3NNYXhIcBgCIAEoBRIQCghib3Nz",
-            "TmFtZRgDIAEoCSI+ChVTX1NwYXduUGxheWVySGF0Y2hlcnkSJQoHcGxheWVy",
-            "cxgBIAMoCzIULlByb3RvY29sLlBsYXllckluZm8iPgoQQ19Nb3ZlQXRIYXRj",
-            "aGVyeRIqCgl0cmFuc2Zvcm0YASABKAsyFy5Qcm90b2NvbC5UcmFuc2Zvcm1J",
-            "bmZvIlAKEFNfTW92ZUF0SGF0Y2hlcnkSEAoIcGxheWVySWQYASABKAUSKgoJ",
-            "dHJhbnNmb3JtGAIgASgLMhcuUHJvdG9jb2wuVHJhbnNmb3JtSW5mbyIOCgxD",
-            "X0F0dGFja0Jvc3MiKAoTU19TZXRIYXRjaGVyeUJvc3NIcBIRCglib3NzQ3Vy",
-            "SHAYASABKAUiDQoLQ19UcnlBdHRhY2siHwoLU19UcnlBdHRhY2sSEAoIcGxh",
-            "eWVySWQYASABKAUq4wUKBU1zZ0lkEgsKB0NfRU5URVIQABILCgdTX0VOVEVS",
-            "EAESCwoHU19TUEFXThACEgsKB0NfTEVBVkUQAxILCgdTX0xFQVZFEAQSDQoJ",
-            "U19ERVNQQVdOEAUSCgoGQ19NT1ZFEAYSCgoGU19NT1ZFEAcSDwoLQ19BTklN",
-            "QVRJT04QCBIPCgtTX0FOSU1BVElPThAJEhQKEENfQ0hBTkdFX0NPU1RVTUUQ",
-            "ChIUChBTX0NIQU5HRV9DT1NUVU1FEAsSCgoGQ19DSEFUEAwSCgoGU19DSEFU",
-            "EA0SEwoPQ19FTlRFUl9EVU5HRU9OEA4SFQoRQ19QTEFZRVJfUkVTUE9OU0UQ",
-            "DxITCg9TX0VOVEVSX0RVTkdFT04QEBITCg9TX0xFQVZFX0RVTkdFT04QERIR",
-            "Cg1TX1NDUkVFTl9URVhUEBISEQoNU19TQ1JFRU5fRE9ORRATEhAKDFNfQkFU",
-            "VExFX0xPRxAUEhMKD1NfU0VUX1BMQVlFUl9IUBAVEhMKD1NfU0VUX1BMQVlF",
-            "Ul9NUBAWEhQKEFNfU0VUX01PTlNURVJfSFAQFxITCg9TX1BMQVlFUl9BQ1RJ",
-            "T04QGBIUChBTX01PTlNURVJfQUNUSU9OEBkSDwoKQ19SRUdJU1RFUhDJARIP",
-            "CgpTX1JFR0lTVEVSEMoBEg0KCENfTE9HX0lOEMsBEg0KCFNfTE9HX0lOEMwB",
-            "EhUKEENfRU5URVJfSEFUQ0hFUlkQzQESFQoQU19FTlRFUl9IQVRDSEVSWRDO",
-            "ARIcChdTX1NQQVdOX1BMQVlFUl9IQVRDSEVSWRDPARIXChJDX01PVkVfQVRf",
-            "SEFUQ0hFUlkQ0AESFwoSU19NT1ZFX0FUX0hBVENIRVJZENEBEhIKDUNfQVRU",
-            "QUNLX0JPU1MQ0gESGwoWU19TRVRfSEFUQ0hFUllfQk9TU19IUBDTARIRCgxD",
-            "X1RSWV9BVFRBQ0sQ1AESEQoMU19UUllfQVRUQUNLENUBQhuqAhhHb29nbGUu",
-            "UHJvdG9idWYuUHJvdG9jb2xiBnByb3RvMw=="));
+            "dGNoZXJ5IpABCg9TX0VudGVySGF0Y2hlcnkSJAoGcGxheWVyGAEgASgLMhQu",
+            "UHJvdG9jb2wuUGxheWVySW5mbxIyChFib3NzVHJhbnNmb3JtSW5mbxgCIAEo",
+            "CzIXLlByb3RvY29sLlRyYW5zZm9ybUluZm8SEQoJYm9zc01heEhwGAMgASgF",
+            "EhAKCGJvc3NOYW1lGAQgASgJIj4KFVNfU3Bhd25QbGF5ZXJIYXRjaGVyeRIl",
+            "CgdwbGF5ZXJzGAEgAygLMhQuUHJvdG9jb2wuUGxheWVySW5mbyI+ChBDX01v",
+            "dmVBdEhhdGNoZXJ5EioKCXRyYW5zZm9ybRgBIAEoCzIXLlByb3RvY29sLlRy",
+            "YW5zZm9ybUluZm8iUAoQU19Nb3ZlQXRIYXRjaGVyeRIQCghwbGF5ZXJJZBgB",
+            "IAEoBRIqCgl0cmFuc2Zvcm0YAiABKAsyFy5Qcm90b2NvbC5UcmFuc2Zvcm1J",
+            "bmZvIg4KDENfQXR0YWNrQm9zcyIoChNTX1NldEhhdGNoZXJ5Qm9zc0hwEhEK",
+            "CWJvc3NDdXJIcBgBIAEoBSINCgtDX1RyeUF0dGFjayIfCgtTX1RyeUF0dGFj",
+            "axIQCghwbGF5ZXJJZBgBIAEoBSrjBQoFTXNnSWQSCwoHQ19FTlRFUhAAEgsK",
+            "B1NfRU5URVIQARILCgdTX1NQQVdOEAISCwoHQ19MRUFWRRADEgsKB1NfTEVB",
+            "VkUQBBINCglTX0RFU1BBV04QBRIKCgZDX01PVkUQBhIKCgZTX01PVkUQBxIP",
+            "CgtDX0FOSU1BVElPThAIEg8KC1NfQU5JTUFUSU9OEAkSFAoQQ19DSEFOR0Vf",
+            "Q09TVFVNRRAKEhQKEFNfQ0hBTkdFX0NPU1RVTUUQCxIKCgZDX0NIQVQQDBIK",
+            "CgZTX0NIQVQQDRITCg9DX0VOVEVSX0RVTkdFT04QDhIVChFDX1BMQVlFUl9S",
+            "RVNQT05TRRAPEhMKD1NfRU5URVJfRFVOR0VPThAQEhMKD1NfTEVBVkVfRFVO",
+            "R0VPThAREhEKDVNfU0NSRUVOX1RFWFQQEhIRCg1TX1NDUkVFTl9ET05FEBMS",
+            "EAoMU19CQVRUTEVfTE9HEBQSEwoPU19TRVRfUExBWUVSX0hQEBUSEwoPU19T",
+            "RVRfUExBWUVSX01QEBYSFAoQU19TRVRfTU9OU1RFUl9IUBAXEhMKD1NfUExB",
+            "WUVSX0FDVElPThAYEhQKEFNfTU9OU1RFUl9BQ1RJT04QGRIPCgpDX1JFR0lT",
+            "VEVSEMkBEg8KClNfUkVHSVNURVIQygESDQoIQ19MT0dfSU4QywESDQoIU19M",
+            "T0dfSU4QzAESFQoQQ19FTlRFUl9IQVRDSEVSWRDNARIVChBTX0VOVEVSX0hB",
+            "VENIRVJZEM4BEhwKF1NfU1BBV05fUExBWUVSX0hBVENIRVJZEM8BEhcKEkNf",
+            "TU9WRV9BVF9IQVRDSEVSWRDQARIXChJTX01PVkVfQVRfSEFUQ0hFUlkQ0QES",
+            "EgoNQ19BVFRBQ0tfQk9TUxDSARIbChZTX1NFVF9IQVRDSEVSWV9CT1NTX0hQ",
+            "ENMBEhEKDENfVFJZX0FUVEFDSxDUARIRCgxTX1RSWV9BVFRBQ0sQ1QFCG6oC",
+            "GEdvb2dsZS5Qcm90b2J1Zi5Qcm90b2NvbGIGcHJvdG8z"));
       descriptor = pbr::FileDescriptor.FromGeneratedCode(descriptorData,
           new pbr::FileDescriptor[] { },
           new pbr::GeneratedClrTypeInfo(new[] {typeof(global::Google.Protobuf.Protocol.MsgId), }, null, new pbr::GeneratedClrTypeInfo[] {
@@ -158,7 +159,7 @@ namespace Google.Protobuf.Protocol {
             new pbr::GeneratedClrTypeInfo(typeof(global::Google.Protobuf.Protocol.C_LogIn), global::Google.Protobuf.Protocol.C_LogIn.Parser, new[]{ "Nickname", "Password" }, null, null, null, null),
             new pbr::GeneratedClrTypeInfo(typeof(global::Google.Protobuf.Protocol.S_LogIn), global::Google.Protobuf.Protocol.S_LogIn.Parser, new[]{ "Success", "Message" }, null, null, null, null),
             new pbr::GeneratedClrTypeInfo(typeof(global::Google.Protobuf.Protocol.C_EnterHatchery), global::Google.Protobuf.Protocol.C_EnterHatchery.Parser, null, null, null, null, null),
-            new pbr::GeneratedClrTypeInfo(typeof(global::Google.Protobuf.Protocol.S_EnterHatchery), global::Google.Protobuf.Protocol.S_EnterHatchery.Parser, new[]{ "Player", "BossMaxHp", "BossName" }, null, null, null, null),
+            new pbr::GeneratedClrTypeInfo(typeof(global::Google.Protobuf.Protocol.S_EnterHatchery), global::Google.Protobuf.Protocol.S_EnterHatchery.Parser, new[]{ "Player", "BossTransformInfo", "BossMaxHp", "BossName" }, null, null, null, null),
             new pbr::GeneratedClrTypeInfo(typeof(global::Google.Protobuf.Protocol.S_SpawnPlayerHatchery), global::Google.Protobuf.Protocol.S_SpawnPlayerHatchery.Parser, new[]{ "Players" }, null, null, null, null),
             new pbr::GeneratedClrTypeInfo(typeof(global::Google.Protobuf.Protocol.C_MoveAtHatchery), global::Google.Protobuf.Protocol.C_MoveAtHatchery.Parser, new[]{ "Transform" }, null, null, null, null),
             new pbr::GeneratedClrTypeInfo(typeof(global::Google.Protobuf.Protocol.S_MoveAtHatchery), global::Google.Protobuf.Protocol.S_MoveAtHatchery.Parser, new[]{ "PlayerId", "Transform" }, null, null, null, null),
@@ -10686,6 +10687,7 @@ namespace Google.Protobuf.Protocol {
     [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
     public S_EnterHatchery(S_EnterHatchery other) : this() {
       player_ = other.player_ != null ? other.player_.Clone() : null;
+      bossTransformInfo_ = other.bossTransformInfo_ != null ? other.bossTransformInfo_.Clone() : null;
       bossMaxHp_ = other.bossMaxHp_;
       bossName_ = other.bossName_;
       _unknownFields = pb::UnknownFieldSet.Clone(other._unknownFields);
@@ -10709,8 +10711,20 @@ namespace Google.Protobuf.Protocol {
       }
     }
 
+    /// <summary>Field number for the "bossTransformInfo" field.</summary>
+    public const int BossTransformInfoFieldNumber = 2;
+    private global::Google.Protobuf.Protocol.TransformInfo bossTransformInfo_;
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
+    public global::Google.Protobuf.Protocol.TransformInfo BossTransformInfo {
+      get { return bossTransformInfo_; }
+      set {
+        bossTransformInfo_ = value;
+      }
+    }
+
     /// <summary>Field number for the "bossMaxHp" field.</summary>
-    public const int BossMaxHpFieldNumber = 2;
+    public const int BossMaxHpFieldNumber = 3;
     private int bossMaxHp_;
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
@@ -10722,7 +10736,7 @@ namespace Google.Protobuf.Protocol {
     }
 
     /// <summary>Field number for the "bossName" field.</summary>
-    public const int BossNameFieldNumber = 3;
+    public const int BossNameFieldNumber = 4;
     private string bossName_ = "";
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
@@ -10749,6 +10763,7 @@ namespace Google.Protobuf.Protocol {
         return true;
       }
       if (!object.Equals(Player, other.Player)) return false;
+      if (!object.Equals(BossTransformInfo, other.BossTransformInfo)) return false;
       if (BossMaxHp != other.BossMaxHp) return false;
       if (BossName != other.BossName) return false;
       return Equals(_unknownFields, other._unknownFields);
@@ -10759,6 +10774,7 @@ namespace Google.Protobuf.Protocol {
     public override int GetHashCode() {
       int hash = 1;
       if (player_ != null) hash ^= Player.GetHashCode();
+      if (bossTransformInfo_ != null) hash ^= BossTransformInfo.GetHashCode();
       if (BossMaxHp != 0) hash ^= BossMaxHp.GetHashCode();
       if (BossName.Length != 0) hash ^= BossName.GetHashCode();
       if (_unknownFields != null) {
@@ -10783,12 +10799,16 @@ namespace Google.Protobuf.Protocol {
         output.WriteRawTag(10);
         output.WriteMessage(Player);
       }
+      if (bossTransformInfo_ != null) {
+        output.WriteRawTag(18);
+        output.WriteMessage(BossTransformInfo);
+      }
       if (BossMaxHp != 0) {
-        output.WriteRawTag(16);
+        output.WriteRawTag(24);
         output.WriteInt32(BossMaxHp);
       }
       if (BossName.Length != 0) {
-        output.WriteRawTag(26);
+        output.WriteRawTag(34);
         output.WriteString(BossName);
       }
       if (_unknownFields != null) {
@@ -10805,12 +10825,16 @@ namespace Google.Protobuf.Protocol {
         output.WriteRawTag(10);
         output.WriteMessage(Player);
       }
+      if (bossTransformInfo_ != null) {
+        output.WriteRawTag(18);
+        output.WriteMessage(BossTransformInfo);
+      }
       if (BossMaxHp != 0) {
-        output.WriteRawTag(16);
+        output.WriteRawTag(24);
         output.WriteInt32(BossMaxHp);
       }
       if (BossName.Length != 0) {
-        output.WriteRawTag(26);
+        output.WriteRawTag(34);
         output.WriteString(BossName);
       }
       if (_unknownFields != null) {
@@ -10825,6 +10849,9 @@ namespace Google.Protobuf.Protocol {
       int size = 0;
       if (player_ != null) {
         size += 1 + pb::CodedOutputStream.ComputeMessageSize(Player);
+      }
+      if (bossTransformInfo_ != null) {
+        size += 1 + pb::CodedOutputStream.ComputeMessageSize(BossTransformInfo);
       }
       if (BossMaxHp != 0) {
         size += 1 + pb::CodedOutputStream.ComputeInt32Size(BossMaxHp);
@@ -10849,6 +10876,12 @@ namespace Google.Protobuf.Protocol {
           Player = new global::Google.Protobuf.Protocol.PlayerInfo();
         }
         Player.MergeFrom(other.Player);
+      }
+      if (other.bossTransformInfo_ != null) {
+        if (bossTransformInfo_ == null) {
+          BossTransformInfo = new global::Google.Protobuf.Protocol.TransformInfo();
+        }
+        BossTransformInfo.MergeFrom(other.BossTransformInfo);
       }
       if (other.BossMaxHp != 0) {
         BossMaxHp = other.BossMaxHp;
@@ -10882,11 +10915,18 @@ namespace Google.Protobuf.Protocol {
             input.ReadMessage(Player);
             break;
           }
-          case 16: {
+          case 18: {
+            if (bossTransformInfo_ == null) {
+              BossTransformInfo = new global::Google.Protobuf.Protocol.TransformInfo();
+            }
+            input.ReadMessage(BossTransformInfo);
+            break;
+          }
+          case 24: {
             BossMaxHp = input.ReadInt32();
             break;
           }
-          case 26: {
+          case 34: {
             BossName = input.ReadString();
             break;
           }
@@ -10916,11 +10956,18 @@ namespace Google.Protobuf.Protocol {
             input.ReadMessage(Player);
             break;
           }
-          case 16: {
+          case 18: {
+            if (bossTransformInfo_ == null) {
+              BossTransformInfo = new global::Google.Protobuf.Protocol.TransformInfo();
+            }
+            input.ReadMessage(BossTransformInfo);
+            break;
+          }
+          case 24: {
             BossMaxHp = input.ReadInt32();
             break;
           }
-          case 26: {
+          case 34: {
             BossName = input.ReadString();
             break;
           }
