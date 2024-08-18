0a052d2c2ebcd2ca137a457b05d6cd2e405b0a88
pledge24 <104922729+pledge24@users.noreply.github.com>
2024-08-18 19:44:58 +0900

    ✨ update. 캐릭터 원거리 공격 구현
    다른 유저의 탄환이 아직 보이지는 않음

--- Assets/Scripts/Dungeon/Attack.cs
+++ Assets/Scripts/Dungeon/Attack.cs
@@ -6,26 +6,58 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     float rate = 1.0f;
-	[SerializeField] BoxCollider meleeArea;
+    [SerializeField] BoxCollider meleeArea;
     [SerializeField] Animator animator;
 
-	bool isAttackReady;
+    [SerializeField] bool isMelee = true;
+    [SerializeField] TrailRenderer trailEffect;
+    [SerializeField] Transform bulletPos;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float bulletSpeed = 50.0f;
+    [SerializeField] float beforeCastDelay = 0.41f;
 
-	float attackDelay;
+    bool isAttackReady;
+    float attackDelay;
+    
 
     IEnumerator TryAttack()
     {
         animator.SetBool("Anim1", true);
 
+        // 다른 유저에게 공격 모션 패킷 전송
         C_TryAttack tryAttackPacket = new C_TryAttack { };
-
         GameManager.Network.Send(tryAttackPacket);
 
-        yield return new WaitForSeconds(0.41f);
+        yield return new WaitForSeconds(beforeCastDelay);
         meleeArea.enabled = true;
 
         yield return new WaitForSeconds(0.1f);
         meleeArea.enabled = false;
+    }
+
+    IEnumerator TryShot()
+    {
+        animator.SetBool("Anim1", true);
+
+        // 다른 유저에게 공격 모션 패킷 전송
+        C_TryAttack tryAttackPacket = new C_TryAttack { };
+        GameManager.Network.Send(tryAttackPacket);
+
+        yield return new WaitForSeconds(beforeCastDelay);
+
+        // 조준선 기준 뱡향벡터 계산.
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        Vector3 targetPoint = Physics.Raycast(ray, out RaycastHit hit) ? hit.point : ray.GetPoint(1000f);
+        Vector3 unitDir = (targetPoint - bulletPos.position).normalized;
+
+        // 탄환 생성 및 발사
+        GameObject instantBullet = Instantiate(bullet, bulletPos.position, bulletPos.rotation);
+
+        Rigidbody bulletRigid = instantBullet.GetComponent<Rigidbody>();
+        bulletRigid.velocity = unitDir * bulletSpeed;
+
+        yield return null;
     }
 
     void Update()
@@ -36,9 +68,19 @@ public class Attack : MonoBehaviour
         var isDead = HatcheryManager.Instance.myPlayer.isDead;
         if (Input.GetButtonDown("Fire1") && isAttackReady && !isDead)
         {
-            StopCoroutine("TryAttack");
-            StartCoroutine("TryAttack");
-            attackDelay = 0.0f;
+            if (isMelee)    // 근거리
+            {
+                StopCoroutine("TryAttack");
+                StartCoroutine("TryAttack");
+                attackDelay = 0.0f;
+            }
+            else           // 원거리
+            {
+                StopCoroutine("TryShot");
+                StartCoroutine("TryShot");
+                attackDelay = 0.0f;
+            }
+
         }
     }
 }
