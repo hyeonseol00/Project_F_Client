defed90cd04fa5bf21a2535915ae36199c72e524
pledge24 <104922729+pledge24@users.noreply.github.com>
2024-08-19 22:03:11 +0900

    🐛 bugfix. 턴 제 몬스터 체력바 오류 해결, item CSV파일 읽을 수 있게 변경

--- Assets/Scripts/Battle/UIMonsterInformation.cs
+++ Assets/Scripts/Battle/UIMonsterInformation.cs
@@ -8,24 +8,25 @@ using UnityEngine.UI;
 public class UIMonsterInformation : MonoBehaviour
 {
     private Transform camTr;
-    
+
     [SerializeField] private GameObject checkArrow;
-    
+
     [SerializeField] private TMP_Text txtName;
     [SerializeField] private TMP_Text txtHp;
     [SerializeField] private Image imgNameBg;
     [SerializeField] private Image imgHpFill;
 
+    public float fillWidth = 180;
+    public float fillHeight = 30;
+
     private float fullHP;
     private float curHP;
-    
-    private float fillWidth = 1000;
-    private float fillHeight = 50;
 
     private void Start()
     {
         if (Camera.main)
             camTr = Camera.main.transform;
+
     }
 
     private void Update()
@@ -35,15 +36,15 @@ public class UIMonsterInformation : MonoBehaviour
 
     public void SetMainCamera()
     {
-		camTr = Camera.main.transform;
-	}
+        camTr = Camera.main.transform;
+    }
 
     public void SetName(string nickname)
     {
         txtName.text = nickname;
         imgNameBg.rectTransform.sizeDelta = new Vector2(txtName.preferredWidth + 50, 50);
     }
-    
+
     public void SetFullHP(float hp, bool recover = true)
     {
         fullHP = hp;
@@ -52,13 +53,13 @@ public class UIMonsterInformation : MonoBehaviour
         if (recover)
             SetCurHP(hp);
     }
-    
+
     public void SetCurHP(float hp)
     {
         curHP = Mathf.Min(hp, fullHP);
         txtHp.text = hp.ToString("0");
-        
-        float per = curHP/fullHP;
+
+        float per = curHP / fullHP;
         imgHpFill.rectTransform.sizeDelta = new Vector2(fillWidth * per, fillHeight);
     }
 
